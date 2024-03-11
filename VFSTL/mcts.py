7b177872a5da8fea3f52a125e4aff0fdcf7e9883
leo <pliu868@connect.hkust-gz.edu.cn>
2024-03-11 08:08:05 +0000

    mcts complete, need to render

--- VFSTL/mcts.py
+++ VFSTL/mcts.py
@@ -1,122 +1,138 @@
 # reference https://ai-boson.github.io/mcts/
 
+from tqdm import tqdm
 import rtamt
 import time
 import numpy as np
+import torch
+import torch.nn as nn
+from stable_baselines3 import PPO
 from collections import defaultdict
 import gym
 import sys
-sys.path.append("/home/leo/Desktop/VFSTL/GCRL-LTL/zones/")
+
+# sys.path.append("/home/leo/Desktop/VFSTL/GCRL-LTL/zones/")
+sys.path.append("/app/vfstl/src/GCRL-LTL/zones")
 from envs import ZoneRandomGoalEnv
 from envs.utils import get_zone_vector
+from collect_skill_trajectories import get_all_goal_value, from_real_dict_to_vector
+from train_dynamics import VFDynamicsMLP
 
-class VFGame:
-    def __init__(self, NNModel, init_state, max_horizon):
-        self.NNModel = NNModel
-        self.init_state = init_state
-        self.cur_state = init_state
-        self.max_horizon = max_horizon
+def stl_cost(states):
+    # from list of tensor, to one tensor
+    states_tensor = torch.zeros([len(states), states[0].size()[0]])         # T x vfs_dim
+    for i in range(len(states)):
+        states_tensor[i, :] = states[i]
 
-    def get_legal_actions(self):
-        '''
-        Modify according to your game or
-        needs. Constructs a list of all
-        possible actions from current state.
-        Returns a list.
-        '''
+    
+    spec = rtamt.StlDiscreteTimeOfflineSpecification()
+    spec.declare_var('J0', 'float')
+    spec.declare_var('W0', 'float')
+    spec.declare_var('R0', 'float')
+    spec.declare_var('Y0', 'float')
+    # spec.spec = 'eventually[0,3]((Y0 >= 0.8) and eventually[0,3](W0 >= 0.8))'
+    spec.spec = 'eventually[0,10](W0 >= 0.8)'
 
-    def is_game_over(self):
-        '''
-        Modify according to your game or
-        needs. It is the game over condition
-        and depends on your game. Returns
-        true or false
-        '''
+    time = torch.arange(0, states_tensor.size()[0])
+    J = states_tensor[:, 0]
+    W = states_tensor[:, 1]
+    R = states_tensor[:, 2]
+    Y = states_tensor[:, 3]
 
-    def game_result(self):
-        '''
-        Modify according to your game or
-        needs. Returns 1 or 0 or -1 depending
-        on your state corresponding to win,
-        tie or a loss.
-        '''
+    dataset = {
+    'time': time,
+    'J0': J,
+    'W0': W,
+    'R0': R,
+    'Y0': Y,
+    }
 
-    def move(self, action):
-        '''
-        Modify according to your game or
-        needs. Changes the state of your
-        board with a new value. For a normal
-        Tic Tac Toe game, it can be a 3 by 3
-        array with all the elements of array
-        being 0 initially. 0 means the board
-        position is empty. If you place x in
-        row 2 column 3, then it would be some
-        thing like board[2][3] = 1, where 1
-        represents that x is placed. Returns
-        the new state after making a move.
-        '''
+    try:
+        spec.parse()
+        #spec.pastify()
+    except rtamt.RTAMTException as err:
+        print('RTAMT Exception: {}'.format(err))
+        sys.exit()
 
+    rob = spec.evaluate(dataset)
+
+    return rob[0][1].item()
 
 class MonteCarloTreeSearchNode:
-    def __init__(self, state, parent=None, parent_action=None):
+    def __init__(self, state, dynamics, max_step, cur_step, parent=None, parent_action=None):
+        self.dynamics = dynamics
+        self.max_step = max_step
+        self.cur_step = cur_step
         self.state = state
         self.parent = parent
         self.parent_action = parent_action
         self.children = []
         self._number_of_visits = 0
-        self._results = defaultdict(int)
-        self._results[1] = 0
-        self._results[-1] = 0
+        # self._results = defaultdict(int)
+        self.score = 0
         self._untried_actions = None
         self._untried_actions = self.untried_actions()
 
     def untried_actions(self):
-        self._untried_actions = self.state.get_legal_actions()
+        self._untried_actions = self.get_legal_actions()
         return self._untried_actions
 
     # need to change to robustness value
-    def q(self):
-        wins = self._results[1]
-        loses = self._results[-1]
-        return wins - loses
+    # def q(self):
+    #     wins = self._results[1]
+    #     loses = self._results[-1]
+    #     return wins - loses
 
     def n(self):
         return self._number_of_visits
 
     def expand(self):
         action = self._untried_actions.pop()
-        next_state = self.state.move(action)
+        next_state = self.move(action, self.state)
         child_node = MonteCarloTreeSearchNode(
-            next_state, parent=self, parent_action=action)
+            next_state, self.dynamics, self.max_step, self.cur_step+1, parent=self, parent_action=action)
 
         self.children.append(child_node)
         return child_node
 
     def is_terminal_node(self):
-        return self.state.is_game_over()
+        if self.cur_step >= self.max_step:
+            return True
+        return False
+        
 
     def rollout(self):
-        current_rollout_state = self.state
+        state_sequences = []
 
-        while not current_rollout_state.is_game_over():
-            possible_moves = current_rollout_state.get_legal_actions()
+        current_rollout_state = self.state
+        state_sequences.append(current_rollout_state)
+        rollout_count = self.cur_step
+        while rollout_count < self.max_step: 
+            possible_moves = self.get_legal_actions()
 
             action = self.rollout_policy(possible_moves)
-            current_rollout_state = current_rollout_state.move(action)
-        return current_rollout_state.game_result()
+            current_rollout_state = self.move(action, current_rollout_state)
+            state_sequences.append(current_rollout_state)
+            rollout_count += 1
 
-    def backpropagate(self, result):
+        return self.game_result(self.traverse_state_seq() + state_sequences)
+
+    def backpropagate(self, reward):
         self._number_of_visits += 1.
-        self._results[result] += 1.
+        self.score += reward
         if self.parent:
-            self.parent.backpropagate(result)
+            self.parent.backpropagate(reward)
 
     def is_fully_expanded(self):
         return len(self._untried_actions) == 0
+    
+    def best_child_max_score(self):
+        choices_weights =[c.score for c in self.children]
+        return self.children[np.argmax(choices_weights)]
 
     def best_child(self, c_param=0.1):
 
-        choices_weights = [(c.q() / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
+        choices_weights = [(c.score / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
         return self.children[np.argmax(choices_weights)]
 
     def rollout_policy(self, possible_moves):
@@ -134,12 +150,138 @@ class MonteCarloTreeSearchNode:
                 current_node = current_node.best_child()
         return current_node
 
-    def best_action(self):
-        simulation_no = 100
+    def build_tree(self, iteration):
 
-        for i in range(simulation_no):
+        for i in tqdm(range(iteration)):
             v = self._tree_policy()
             reward = v.rollout()
             v.backpropagate(reward)
 
-        return self.best_child(c_param=0.)
+        # return self.best_child(c_param=0.)
+
+    def move(self, action, state):
+        return self.dynamics.forward_simulation(action, state)
+
+    # def is_game_over(self):
+    #     '''
+    #     Modify according to your game or
+    #     needs. It is the game over condition
+    #     and depends on your game. Returns
+    #     true or false
+    #     '''
+    #     if self.cur_step >= self.max_step:
+    #         return True
+    #     return False
+
+    def get_legal_actions(self):
+        '''
+        Modify according to your game or
+        needs. Constructs a list of all
+        possible actions from current state.
+        Returns a list.
+        '''
+
+        return list(range(self.dynamics.size_discrete_actions))
+
+    def game_result(self, states_sequence):
+        '''
+        Modify according to your game or
+        needs. Returns 1 or 0 or -1 depending
+        on your state corresponding to win,
+        tie or a loss.
+        '''
+        return stl_cost(states_sequence)
+
+    def traverse_state_seq(self):
+        states_sequence = []
+        tmp = self
+        # record state values and time
+        while tmp.parent:
+            tmp = tmp.parent
+            # record state values and time
+            states_sequence.append(tmp.state)
+
+        return states_sequence[::-1]
+
+
+class VFDynamics:
+
+    def __init__(self, NNModel, size_discrete_actions) -> None:
+        self.NNModel = NNModel
+        self.size_discrete_actions = size_discrete_actions
+
+    def forward_simulation(self, control, vfs):
+        # one hot control, concate with vfs, call model forward
+        control = nn.functional.one_hot(torch.tensor(control, dtype=torch.int64), num_classes=vfs.size()[0])
+        nn_input = torch.concatenate((control, vfs), 0)
+        return self.NNModel.predict(nn_input.to(torch.float32))
+
+
+
+def best_action_sequence(root):
+    action_sequence = []
+    states_sequence = []
+    cur = root
+    states_sequence.append(cur.state)
+    while len(cur.children) > 0:
+        cur = cur.best_child_max_score()
+        states_sequence.append(cur.state)
+        action_sequence.append(cur.parent_action)
+
+    return action_sequence, states_sequence
+
+if __name__ == '__main__':
+    device = torch.device("cpu")
+
+    # lower level policy
+    # model_path = '../GCRL-LTL/zones/models/goal-conditioned/best_model_ppo_8'
+    model_path = '/app/vfstl/src/GCRL-LTL/zones/models/goal-conditioned/best_model_ppo_8'
+    sys.path.append("/app/vfstl/src/GCRL-LTL/zones")
+    policy_model = PPO.load(model_path, device=device)
+    timeout = 10000
+    env = ZoneRandomGoalEnv(
+        env=gym.make('Zones-8-v0', timeout=timeout),
+        primitives_path='/app/vfstl/src/GCRL-LTL/zones/models/primitives', 
+        goals_representation=get_zone_vector(),
+        use_primitves=True,
+        rewards=[0, 1],
+    )
+
+    # upper level policy
+    vf_num = 4
+    max_step = 10
+    model = VFDynamicsMLP(vf_num).to(device=device)
+    model.load_state_dict(torch.load('/app/vfstl/src/VFSTL/dynamic_models/test_model_20240307_085639_11', map_location=device))
+    dynamic = VFDynamics(model, vf_num)
+
+    obs = env.reset()
+    init_state = torch.from_numpy(from_real_dict_to_vector(get_all_goal_value(obs, policy_model.policy, get_zone_vector(), device))).to(device)
+
+    root = MonteCarloTreeSearchNode(init_state, dynamic, max_step, cur_step=0)
+    root.build_tree(50000)
+    controls, states = best_action_sequence(root)
+
+    print(controls)
+    print(states)
+
+    # plot
+    # T_horizon = 10
+    # skill_timesteps = 100
+    # env.render()
+    # for i in range(0, T_horizon):
+    #     frame = env.fix_goal(env.goals[controls[i]])
+    #     print(np.shape(frame))
+    #     for j in range(0, skill_timesteps):
+    #         action, _ = policy_model.predict(env.current_observation(), deterministic=True)
+    #         obs, reward, eval_done, info = env.step(action)
+    #         env.render()
+    #         time.sleep(0.0001)
+    #     real_value = torch.from_numpy(from_real_dict_to_vector(get_all_goal_value(obs, policy_model.policy, get_zone_vector(), device))).to(device)
+    #     print('------------real value--------------')
+    #     print(real_value)
+
+
+
+
+
+
